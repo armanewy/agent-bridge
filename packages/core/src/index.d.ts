48469724cb1c469b26fa000a167caac2c8f3508e
Arman Ozturk <aozturk6@gmail.com>
2026-05-25 20:44:19 -0400

    chore: remove generated core artifacts

--- packages/core/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types.js";
-export * from "./adapters.js";
-export * from "./handoff.js";
-export * from "./redaction.js";
-//# sourceMappingURL=index.d.ts.map
--- packages/core/src/redaction.d.ts
+++
@@ -1,5 +0,0 @@
-import type { RedactionFinding } from "./types.js";
-export declare function detectRedactions(text: string): RedactionFinding[];
-export declare function applyRedactions(text: string, findings: RedactionFinding[]): string;
-export declare function hasHighSeverityFinding(findings: RedactionFinding[]): boolean;
-//# sourceMappingURL=redaction.d.ts.map
